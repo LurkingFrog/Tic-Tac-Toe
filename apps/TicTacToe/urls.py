c9ece401142f5930d993bb7c1f02226f4085065b
Dave Fogelson <github@landfillinc.com>
2012-10-03 11:46:36 -0400

    Created the html to display a game board. Added the images used on there.
    All that's left is the javascript to make it go.

--- apps/TicTacToe/urls.py
+++ apps/TicTacToe/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,3 +11,5 @@ urlpatterns = patterns(
     url(r'move/(?P<board_id>\d{9})/$', 'ajax_get_move', name='get_move'),
     
 )
+
+urlpatterns += staticfiles_urlpatterns()
